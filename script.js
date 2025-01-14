040753d26a00e6ad90b6f1646c46a2f69e4efa96
kishan <vishwakarmakishan620@gmail.com>
2025-01-14 12:33:22 +0530

    add

--- script.js
+++ script.js
@@ -21,4 +21,47 @@ function disclose(){
     document.querySelector(".booking-container").style.display = "none";
 }
 
+async function book_table(){
+    let data =fetch("http://localhost:3000/Booking");
+    let data2 =await data;
+    let data3 =await data2.json(); 
+    console.log(data3)
+   
+    let date =document.querySelector('#date').value;
+    let time = document.querySelector('#time').value;
+    let guest =document.querySelector("#guests").value;
+    let table=document.querySelector("#tablenumber").value;
+    if(date === ""){
+        return false;
+    }
+    else if (time === ''){
+        return false;
+    }
+    else if (guest ===""){
+        return false;
+    }
+    else if (table ===""){
+        return false;
+    }
+
+    let booking_data ={
+        Date:date,
+        time:time,
+        Guest:guest,
+        Table:table,
+        request:document.querySelector("#requestbox").value
+    }
+    
+    fetch("http://localhost:3000/Booking",{
+        method:"POST",
+        headers:{
+            'Content_type': 'application/json',
+        },
+        body:JSON.stringify(booking_data)
+    })
+    .then(rev=>alert("Your table is booked !!!!!!"));
+    console.log(booking_data);
+    return true;
+    
+}
 
